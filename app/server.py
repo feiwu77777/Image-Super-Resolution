c23021e91cf184efe2da5f11af0224eaa12a8b4c
Fei WU <feiwu75009@hotmail.fr>
2019-10-23 20:30:52 +0200

    a

--- app/server.py
+++ app/server.py
@@ -1,16 +1,4 @@
-from flask import Flask, render_template, request
-from flask_uploads import UploadSet, configure_uploads,IMAGES
-from flask import send_from_directory
-#for matrix math
-import numpy as np
-from werkzeug import secure_filename
-#for regular expressions, saves time dealing with string data
-import re
-#system level operations (like loading files)
 import sys
-#for reading operating system data
-import os
-#initalize our flask app
 import uvicorn
 
 import fastai
@@ -20,7 +8,17 @@ from fastai.utils.mem import *
 
 import asyncio, aiohttp
 
-app = Flask(__name__)
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+from starlette.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory='app/templates')
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
 
 path = Path(__file__).parent #app/
 
@@ -47,31 +45,27 @@ tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
-def api(full_path):
-    img = open_image(full_path)
-    print('image opened')
-    p, predict, b = learn.predict(img)
-    return p
-
-photos = UploadSet('photos', IMAGES)
-saving_path = './static/user_imgs/uploaded/'
-app.config['UPLOADED_PHOTOS_DEST'] = saving_path
-configure_uploads(app, photos)
-
 @app.route('/')
-def index():
-	return render_template("index.html")
+def index(request):
+    index_html = path/'templates'/'index.html'
+    return HTMLResponse(index_html.open().read())
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if (request.method == 'POST') and ('photo' in request.files):
-        filename = photos.save(request.files['photo'])
+@app.route('/upload', methods=['POST'])
+async def upload(request):
+    data = await request.form()
+    img_bytes = await (data["photo"].read())
+    img = open_image(BytesIO(img_bytes))
+    uploaded_name = 'app/static/user_imgs/uploaded/test.jpg'
+    img.save(uploaded_name)
 
-    saved_path = saving_path + filename
-    result = api(saved_path)
-    saved_name = './static/user_imgs/computed/' + filename
-    result.save(saved_name)
-    return render_template("index2.html", uploaded_path = saved_path, computed_path = saved_name)
+    p, _, _ = learn.predict(img)
+
+    computed_name = 'app/static/user_imgs/computed/test.jpg'
+    p.save(computed_name)
+    return templates.TemplateResponse('index2.html', 
+            {'request': request, 
+             'uploaded_path': uploaded_name[4:], 
+             'computed_path' : computed_name[4:]})
 
 if __name__ == "__main__":
     if 'serve' in sys.argv:
